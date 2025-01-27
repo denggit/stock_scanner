d2ab624e0dbb61591933b5f1d5d71dfba7d92334
Zijun Deng <Zijun_deng@163.com>
2025-01-27 17:12:58 +0800

    Initial commit

---
+++ backend/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author     : Zijun Deng
+@Date       : 1/27/2025 4:42 PM
+@File       : __init__.py.py
+@Description: 
+"""
---
+++ run_frontend.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author     : Zijun Deng
+@Date       : 1/27/2025 4:49 PM
+@File       : run_frontend.py
+@Description: 
+"""
